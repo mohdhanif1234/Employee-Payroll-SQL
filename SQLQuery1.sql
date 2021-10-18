39f2dee820029a274c8128ad32eadb749efc7d42
mohdhanif1234 <hanif.topia@gmail.com>
2021-10-18 07:13:56 +0530

    [Hanif]:Added a SQL query to retrieve a specific data

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -10,7 +10,16 @@ create table employee_payroll(
 	);
 
 --UC-3: Performing CRUD operations
-insert into employee_payroll values('Varun','125245','2016/03/01') ,('Hanif','13335','2018/01/05'),('Deeptimayee','525245','2019/03/01'),('Ameeja','541236','2016/11/29'),('Naveen','147853','2020/07/14');
+insert into employee_payroll values
+('Varun','125245','2016/03/01') ,
+('Hanif','13335','2018/01/05'),
+('Deeptimayee','525245','2019/03/01'),
+('Ameeja','541236','2016/11/29'),
+('Naveen','147853','2020/07/14');
 
 --UC-4: Retrieve all data
 select * from employee_payroll;
+
+--UC-5: Retrieving specific data
+select Name,StartDate from employee_payroll where Name='Hanif';
+select * from employee_payroll where StartDate between cast('2005-01-01' as date) and getdate();
